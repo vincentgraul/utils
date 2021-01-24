07b25b2f22aa7c63a4274961af35ea967452f13f
Vincent Graul <vincent.graul@outlook.com>
2021-01-24 09:49:04 +0100

    feat(fs): add fs util

---
+++ packages/fs/src/fs.ts
@@ -0,0 +1,43 @@
+import { basename } from "path";
+import { Dirent, readdir } from "fs-extra";
+import slash from "slash";
+
+export interface Path {
+  absolute: string;
+  relative: string;
+}
+
+export interface Folder {
+  name: string;
+  path: Path;
+}
+
+export async function listFolders(path: string, excludes: string[] = []): Promise<Folder[]> {
+  const folders: Folder[] = [];
+
+  const readFolder = async (currentPath: Path) => {
+    const dirents: Dirent[] = await readdir(currentPath.absolute, {
+      withFileTypes: true,
+    });
+
+    for (const dirent of dirents) {
+      if (dirent.isDirectory()) {
+        const { name } = dirent;
+        const path: Path = {
+          absolute: `${currentPath.absolute}/${name}`,
+          relative: `${currentPath.relative}/${name}`,
+        };
+
+        folders.push({ name, path });
+
+        if (!excludes.includes(name)) {
+          await readFolder(path);
+        }
+      }
+    }
+  };
+
+  await readFolder({ absolute: slash(path), relative: basename(slash(path)) });
+
+  return folders;
+}
