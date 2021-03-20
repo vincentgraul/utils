e5d1f0aa06f662aa122166164842cc1230430892
Vincent Graul <vincent.graul@outlook.com>
2021-03-20 19:15:48 +0100

    feat: add platform util

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ## Utils for browser or Node.js
 
-| Util | Description                                  |
-| ---- | -------------------------------------------- |
-| fs   | A fs util that adds some missing functions   |
-| npm  | A npm util that adds some missing functions  |
-| path | A path util that adds some missing functions |
+| Util     | Description                                                   |
+| -------- | ------------------------------------------------------------- |
+| fs       | A fs util that adds some missing functions                    |
+| npm      | An npm util to use npm commands programmatically              |
+| path     | A path util that adds some useful functions                   |
+| platform | A platform util to know in which environment code is executed |
 
 ## License
 
---
+++ src/platform.ts
@@ -0,0 +1,53 @@
+export enum Platform {
+  Browser = "browser",
+  Server = "server",
+}
+
+export interface Exception {
+  origin: string;
+  message?: string;
+}
+
+export function isBrowser(): boolean {
+  return typeof window !== "undefined";
+}
+
+export function isServer(): boolean {
+  return typeof window === "undefined";
+}
+
+export function isBrowserOrThrowException(
+  e: Exception = getDefaultException(Platform.Browser)
+): boolean {
+  const value: boolean = isBrowser();
+
+  if (!value) {
+    throw new Error(formatException(e, Platform.Browser));
+  }
+
+  return value;
+}
+
+export function isServerOrThrowException(
+  e: Exception = getDefaultException(Platform.Server)
+): boolean {
+  const value: boolean = isServer();
+
+  if (!value) {
+    throw new Error(formatException(e, Platform.Server));
+  }
+
+  return value;
+}
+
+function getDefaultException(platform: Platform): Exception {
+  return { origin: "this function", message: `is not available in ${platform} environment` };
+}
+
+function formatException(exception: Exception, platform: Platform): string {
+  const message: string = exception.message
+    ? exception.message
+    : getDefaultException(platform).message;
+
+  return `${exception.origin} ${message}`;
+}
