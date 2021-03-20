60f15800321124cb7db84cbce11d1127ddc4d4a7
Vincent Graul <vincent.graul@outlook.com>
2021-03-20 19:22:30 +0100

    build: add webpack config

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as fs from "./fs";
+import * as npm from "./npm";
+import * as path from "./path";
+import * as platform from "./platform";
+
+export default { fs, npm, path, platform };
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { join } = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "production",
+  entry: join(__dirname, "src/index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  externals: [
+    nodeExternals({
+      modulesDir: join(__dirname, "node_modules"),
+    }),
+  ],
+  output: {
+    filename: "index.js",
+    path: join(__dirname, "build"),
+    library: "utils",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    umdNamedDefine: true,
+    globalObject: "this",
+  },
+};
