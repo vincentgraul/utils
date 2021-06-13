6230a9d47e93aed7e919d2ae9767019cc906dc6f
Vincent Graul <vincent.graul@outlook.com>
2021-06-13 23:44:18 +0200

    feat: decompose file path

--- src/path.ts
+++ src/path.ts
@@ -1,6 +1,19 @@
 import slash from "slash";
 
+export interface File {
+  name: string;
+  extension: string;
+  path: string;
+}
+
 export function getLastPath(path: string): string {
   const formattedPath: string = slash(path);
   return formattedPath.substring(formattedPath.lastIndexOf("/") + 1);
+}
+
+export function decomposeFilePath(path: string): File {
+  const details: string[] = path.split("/").pop().split(".");
+  const extension: string = details.pop();
+
+  return { name: details.join("."), extension, path };
 }
